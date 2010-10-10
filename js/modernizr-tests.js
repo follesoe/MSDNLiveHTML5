fc70aefbcea8bffdaea0a8c9f89ed4b195df190e
Jonas Folleso <jonas@follesoe.no>
2010-10-10 21:19:18 +0200

    More work on audio player and feature detection for pinned sites.

--- js/modernizr-tests.js
+++ js/modernizr-tests.js
@@ -1,6 +1,16 @@
-﻿Modernizr.addTest("pinnedsite", function() {
+﻿Modernizr.addTest("insitemode", function() {
     try {
         return window.external.msIsSiteMode();
+    }
+    catch(e) {       
+        return false; 
+    }
+});
+
+Modernizr.addTest("sitemode", function() {
+    try {
+        window.external.msIsSiteMode();
+        return true;
     }
     catch(e) {       
         return false; 
