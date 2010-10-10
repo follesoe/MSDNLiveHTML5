79dc470eb5e2babbcc80879d82d8e049d9f0c59b
Jonas Folleso <jonas@follesoe.no>
2010-10-10 19:19:04 +0200

    Added feature detection for IE9 pinned sites.

---
+++ js/modernizr-tests.js
@@ -0,0 +1,12 @@
+﻿Modernizr.addTest("pinnedsite", function() {
+    if (window.external.msIsSiteMode) {
+        try {
+            return window.external.msIsSiteMode();
+        }
+        catch(e) {       
+            return fale; 
+        }
+    } else { 
+        return false; 
+    };
+});
